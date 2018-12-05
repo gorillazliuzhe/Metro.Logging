348f32da537c09aebbb5853b2b2d7a9ac6b8b237
LZ <liuzhellll@hotmail.com>
2018-12-05 11:47:43 +0800

    升级2.2

--- samples/Metro.Logging.File.Sample/Startup.cs
+++ samples/Metro.Logging.File.Sample/Startup.cs
@@ -19,7 +19,7 @@ namespace Metro.Logging.File.Sample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddLoggingFileUI();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
